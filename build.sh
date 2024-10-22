4677d61016134efc92b49bae3db5f566090e6fec
Thomas Off <thomas.off@netzarbeiter.info>
2024-10-23 00:32:55 +0200

    Set up multi-arch build.

--- build.sh
+++ build.sh
@@ -17,19 +17,37 @@ echo ""
 # Define PHP versions.
 php_versions=(7.4 8.0 8.1 8.2 8.3)
 
-# Build and push images.
-for php_version in "${php_versions[@]}"
-do
-    echo "Building image for PHP $php_version"
+# Define function for building and pushing images.
+function build_and_push_image {
+    php_version=$1
+    platform=$2
+    suffix=$3
+
+    echo "Building image for PHP $php_version on platform $platform"
     rm -rf $php_version && mkdir $php_version
     cp Dockerfile.template $php_version/Dockerfile
     sed -i '' "s/%PHP-VERSION%/$php_version/" $php_version/Dockerfile
     sed -i '' "s/%DEPLOYER-VERSION%/$deployer_version/" $php_version/Dockerfile
-    docker build --file $php_version/Dockerfile -t netzarbeiter/deployer:php-$php_version-deployer-$deployer_version -t netzarbeiter/deployer:php-$php_version .
+    docker build --platform $platform --file $php_version/Dockerfile -t netzarbeiter/deployer-$suffix:php-$php_version-deployer-$deployer_version -t netzarbeiter/deployer-$suffix:php-$php_version .
     echo ""
 
-    echo "Pushing image for PHP $php_version"
-    docker push netzarbeiter/deployer:php-$php_version-deployer-$deployer_version
-    docker push netzarbeiter/deployer:php-$php_version
+    echo "Pushing image for PHP $php_version on platform $platform"
+    docker push netzarbeiter/deployer-$suffix:php-$php_version-deployer-$deployer_version
+    docker push netzarbeiter/deployer-$suffix:php-$php_version
     echo ""
+}
+
+# Build and push images.
+for php_version in "${php_versions[@]}"
+do
+    # Build and push platform-specific images.
+    build_and_push_image $php_version linux/arm64 arm64
+    build_and_push_image $php_version linux/amd64 amd64
+
+    # Create and push multi-arch manifest.
+    echo "Creating and pushing multi-arch manifest for PHP $php_version"
+    docker manifest create netzarbeiter/deployer:php-$php_version-deployer-$deployer_version netzarbeiter/deployer-arm64:php-$php_version-deployer-$deployer_version netzarbeiter/deployer-amd64:php-$php_version-deployer-$deployer_version
+    docker manifest push netzarbeiter/deployer:php-$php_version-deployer-$deployer_version
+    docker manifest create netzarbeiter/deployer:php-$php_version netzarbeiter/deployer-arm64:php-$php_version netzarbeiter/deployer-amd64:php-$php_version
+    docker manifest push netzarbeiter/deployer:php-$php_version
 done
